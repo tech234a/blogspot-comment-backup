31c303f85a637976a4cf29636d3ba3aa662d834b
afrmtbl <afrmtbl@gmail.com>
2019-02-16 00:31:37 -0500

    Switch to requests sessions (+renaming)

--- blogspot-comment-backup.py
+++ blogspot-comment-backup.py
@@ -1,29 +1,34 @@
 import requests, json
 
-def getbloginfo(blog):
+def get_blog_info(blog):
+
+    #Create a new request session so we can reuse for following requests
+    #Results in much faster requests
+    session = requests.Session()
+
     #Initialize variables for the while loop
     i = 0
     post_urls = []
     complete = False
-    while complete == False:
+    while not complete:
         #Can only get 150 blog posts returned even if a higher number is specified
         url = blog + '/feeds/posts/default?max-results=150&alt=json&start-index=' + str((i * 150) + 1)
         print("getting url: " + url)
-        requestinfo = requests.get(url)
+        request_info = session.get(url)
         #Check if the blog exists and is accessible
-        if requestinfo.status_code == 404: #Blog does not exist
+        if request_info.status_code == 404: #Blog does not exist
             return "nf" #Blog not found
-        elif requestinfo.status_code == 401: #Blog is private. Note: Blogs with content warnings do not seem to be blocked by these requests, so this error will *not* appear for those.
+        elif request_info.status_code == 401: #Blog is private. Note: Blogs with content warnings do not seem to be blocked by these requests, so this error will *not* appear for those.
             return "pr" #Private blog
-        elif not requestinfo.ok: #Any other error. Should really retry these requests.
+        elif not request_info.ok: #Any other error. Should really retry these requests.
             return "oe" #Other error
         else: #The blog is accessible, proceed in retreiving links
-            feed_json = json.loads(requestinfo.text)
+            feed_json = json.loads(request_info.text)
             post_urls.extend([feed_json['feed']['entry'][i]['link'][-1]['href'] for i in range(0, len(feed_json['feed']['entry']))])
             if len(feed_json['feed']['entry']) != 150:
                 complete = True
             else:
-                i+=1
+                i += 1
     return post_urls #Return the complete list of articles
 
 if __name__ == '__main__':
@@ -31,7 +36,7 @@ if __name__ == '__main__':
 	#A trailing slash on the URL seems to work OK, even if it processes with a double slash
 	blog = 'https://googleblog.blogspot.com'#'https://blogger.googleblog.com'#'https://mytriptoamerica.blogspot.com'
 
-	post_urls = getbloginfo(blog) #Retrieve the sample blog's articles
+	post_urls = get_blog_info(blog) #Retrieve the sample blog's articles
 	print(f"Found {len(post_urls)} post links")
 	print(post_urls)
 
